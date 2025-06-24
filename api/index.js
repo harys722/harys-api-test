2ab396f4f4c5828a21ee995671d4f8e5442aa675
- HARYs - <174037813+harys722@users.noreply.github.com>
2025-06-24 16:20:08 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
-module.exports = (req, res) => {
-  res.writeHead(302, { Location: 'https://harys.is-a.dev/' });
-  res.end();
-};
+export default function handler(req, res) {
+
+  res.json({
+    success: "false",
+    error: "The requested resource does not exist",
+    message:"Make sure you are sending a request to a valid endpoint or web-page",
+    info: {
+      credits: "Made by harys722, available only for cool people.",
+      support: "https://harys.is-a.dev/api"
+    }
+  });
+}
