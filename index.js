a564a3cde7dd6852b92d5ecebbc41d8e8e382825
- HARYs - <174037813+harys722@users.noreply.github.com>
2025-07-01 14:40:29 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/api/v1', (req, res) => {
+  res.send('API is running');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
